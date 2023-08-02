02cb78570d89a4e39e68c2031de198e5de617485
DEBORAH_ACRI <a.debbie98@gmail.com>
2023-08-02 09:01:42 -0400

    First commit

--- project/app.py
+++ project/app.py
@@ -17,7 +17,7 @@ cl = Client()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
+=
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -55,7 +55,7 @@ def login():
         return render_template("ban.html")
 
     else:
-        return render_template("login.html")
+        return render_template("index.html")
 
 @app.route("/ban", methods=["GET", "POST"])
 @login_required
